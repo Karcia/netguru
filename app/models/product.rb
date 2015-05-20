aa954ae77f2162ec7eac5f2b878b16f76beadb3b
Karcia <karciawow@gmail.com>
2015-05-21 01:20:26 +0200

    working on average_rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,5 +8,20 @@ class Product < ActiveRecord::Base
   validates_presence_of :description
   validates_presence_of :title
 
+  def average_rating_full
+  	ratings = []
+  	reviews.each do |r|
+  		ratings << r.rating
+  	end
+  	sum = 0
+  	ratings.each do |ra|
+  		sum += ra
+  	end
+	sum.to_f / ratings.count
+  end
+
+  def average_rating
+  	sum = reviews.collect{|r| r.rating}.inject(:+)
+  	sum.to_f / reviews.count
+  end
 end
-# /^\d+\.*\d{0,2}$/
